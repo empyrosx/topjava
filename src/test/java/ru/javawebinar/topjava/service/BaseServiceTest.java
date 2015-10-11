e450507cf5fbcbb85903cb6458dca336b34ed92c
EmpyrosX <EmpyrosX@yandex.ru>
2015-10-11 23:37:03 +0300

    HW5-required

---
+++ src/test/java/ru/javawebinar/topjava/service/BaseServiceTest.java
@@ -0,0 +1,22 @@
+package ru.javawebinar.topjava.service;
+
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static ru.javawebinar.topjava.Profiles.POSTGRES;
+
+@ContextConfiguration({
+        "classpath:spring/spring-app.xml",
+        "classpath:spring/spring-db.xml"
+})
+@RunWith(SpringJUnit4ClassRunner.class)
+@Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
+@ActiveProfiles(POSTGRES)
+@Ignore
+public class BaseServiceTest {
+}
